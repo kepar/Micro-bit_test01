9153bad3ba64a5e49c1142acbcdf81b7bd470d68
kepar <1457302+kepar@users.noreply.github.com>
2021-01-02 17:21:51 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,77 +1,59 @@
-let pause2 = 0
-input.onPinPressed(TouchPin.P0, function () {
-    led.plot(0, 0)
-})
+let pauze = 0
+let playJingleBells = false
 function spiraal () {
-    pause2 = 200
-    basic.pause(pause2)
+    basic.clearScreen()
+    pauze = 100
+    basic.pause(pauze)
     led.toggle(0, 0)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(1, 0)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(2, 0)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(3, 0)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(4, 0)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(4, 1)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(4, 2)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(4, 3)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(4, 4)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(3, 4)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(2, 4)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(1, 4)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(0, 4)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(0, 3)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(0, 2)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(0, 1)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(1, 1)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(2, 1)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(3, 1)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(3, 2)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(3, 3)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(2, 3)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(1, 3)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(1, 2)
-    basic.pause(pause2)
+    basic.pause(pauze)
     led.toggle(2, 2)
 }
-input.onButtonPressed(Button.A, function () {
-    for (let y = 0; y <= 4; y++) {
-        // basic.pause(200)
-        // led.toggle(x, y)
-        for (let x = 0; x <= 4; x++) {
-            basic.pause(200)
-            led.toggle(x, y)
-        }
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    // JingleBells()
-    spiraal()
-    input.onPinPressed(TouchPin.P0, function () {
-    JingleBells()
-})
-})
 function JingleBells () {
     music.playTone(330, music.beat(BeatFraction.Half))
     music.rest(music.beat(BeatFraction.Eighth))
@@ -122,5 +104,23 @@ function JingleBells () {
     music.playTone(262, music.beat(BeatFraction.Whole))
 }
 basic.forever(function () {
-	
+    while (input.lightLevel() > 4) {
+        basic.showNumber(3)
+        basic.pause(1000)
+        basic.showNumber(2)
+        basic.pause(1000)
+        basic.showNumber(1)
+        basic.pause(1000)
+        playJingleBells = true
+        spiraal()
+        basic.showString("Gelukkig Nieuwjaar!")
+        playJingleBells = false
+        basic.clearScreen()
+        basic.pause(10000)
+    }
+})
+basic.forever(function () {
+    if (playJingleBells) {
+        JingleBells()
+    }
 })
